7cbedbeb83965ad865ab1239a191b612833061cb
Lily Lyons <lily@nowaffles.com>
2023-02-08 21:09:43 -0800

    Fix an issue with options

--- src/de/deserializer.rs
+++ src/de/deserializer.rs
@@ -345,9 +345,19 @@ impl<'de, 'a> de::Deserializer<'de> for &'a mut Deserializer<'de> {
         }
     }
 
+    fn deserialize_option<V>(self, visitor: V) -> std::result::Result<V::Value, Self::Error>
+    where
+        V: de::Visitor<'de>,
+    {
+        match self.peek_tag()? {
+            Tag::Nil => visitor.visit_none(),
+            _ => visitor.visit_some(self),
+        }
+    }
+
     forward_to_deserialize_any! {
         bool i8 i16 i32 i64 i128 u8 u16 u32 u64 u128 f32 f64 char str string
-        bytes byte_buf option unit unit_struct newtype_struct seq tuple
+        bytes byte_buf unit unit_struct newtype_struct seq tuple
         tuple_struct map struct enum identifier ignored_any
     }
 }
