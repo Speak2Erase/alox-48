ccde75a895a1fe882459863965b2905086368123
Speak2Erase <lily@nowaffles.com>
2024-05-29 17:04:13 -0700

    Relicense under mpl

--- alox-48/src/de/error.rs
+++ alox-48/src/de/error.rs
@@ -1,19 +1,8 @@
-// Copyright (C) 2022 Lily Lyons
+// Copyright (c) 2024 Lily Lyons
 //
-// This file is part of alox-48.
-//
-// alox-48 is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// alox-48 is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with alox-48.  If not, see <http://www.gnu.org/licenses/>.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
 #![allow(missing_docs)]
 
 use std::str::Utf8Error;
